e7324a5c8f53287d902df9f2504f675b40c6ad34
Šime Vidas <sime.vidas@gmail.com>
2015-01-01 20:48:21 +0100

    Overhaul of API

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
-// extends https://github.com/efacilitation/gulp-modify
+'use strict';
 
-var through = require('through2')
+var gutil = require('gulp-util');
+var through = require('through2');
 
-module.exports = function(modifier){
-  return through.obj(function(file, enc, next){
-    if (file.isBuffer() && modifier) {
-      var src = file.contents.toString()
-      // this === file
-      modifier.apply(file, [src, function(err, result){
-        file.contents = new Buffer(result)
-        next(err, file)
-      }])
-    } else next(null, file)
-  })
-}
+module.exports = function (modifier) {
+	
+	return through.obj(function (file, enc, next) {
+		
+		function buffer(contents) {
+			file.contents = new Buffer(contents);
+			next(null, file);
+
+		}
+	  
+		if (file.isNull() || !modifier) {
+			next(null, file);
+			return;
+		}
+
+		if (file.isStream()) {
+			cb(new gutil.PluginError('gulp-edit', 'Streaming not supported'));
+			return;
+		}
+		
+		try {
+			var retVal = modifier.apply(file, [
+				file.contents.toString(),
+				file,
+				buffer
+			]);
+			
+			if (retVal) {
+				buffer(retVal);
+			}
+			
+		} catch (err) {
+			next(new gutil.PluginError('gulp-edit', err, { fileName: file.path }));
+		}
+  });
+};
